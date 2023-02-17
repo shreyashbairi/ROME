5a09c6b17bd21d571b24856219b7139261aa1e81
Joy Kramer <ejkramer21@gmail.com>
2023-02-16 23:00:35 -0500

    cleaning repo

---
+++ client/src/components/jsx/Welcome.jsx
@@ -0,0 +1,19 @@
+import '../css/Welcome.css';
+
+function Welcome () {
+  return (
+    <div calss="Welcome-page">
+        <h1 class="Welcome-header">Weclome to Rome</h1>
+        <div class="Welcome-buttons">
+            <a href="/login">
+                <button type="button" class="btn btn-secondary">Login</button>
+            </a>
+            <a href="/signup">
+                <button type="button" class="btn btn-secondary">Signup</button>
+            </a>
+        </div>
+    </div>
+  );
+}
+
+export default Welcome;
