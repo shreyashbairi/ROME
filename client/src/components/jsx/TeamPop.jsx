975cd7fcdda7039dc32e1f72267efecdab0156c1
willcq12 <willcq12@gmail.com>
2023-02-28 20:46:30 -0500

    minor fix

--- client/src/components/jsx/TeamPop.jsx
+++ client/src/components/jsx/TeamPop.jsx
@@ -16,17 +16,6 @@ function TeamPop(props) {
 
     async function handleSubmit(e) {
         e.preventDefault();
-<<<<<<< HEAD
-        try{
-            await axios.post('/Submit', {
-              team,
-              description
-            });
-            alert("Team Successfully Created.  Redirecting you now.");
-          } catch (e){
-            alert('Team Creation Failed. Please try again later.')
-          }
-=======
         // try{
         //     await axios.post('/Submit', {
         //       team,
@@ -42,7 +31,6 @@ function TeamPop(props) {
             description: description
         });
         props.setTrigger(false);
->>>>>>> 0c89cd0e12e97e383a399fab5a6f1f9385aecd76
         setTeam("");
         setDescription("");
     }
@@ -67,13 +55,6 @@ function TeamPop(props) {
 
     const openTeamPage = e => {
         return (
-<<<<<<< HEAD
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/team" element={<TeamHome />}/>
-                </Routes>
-            </BrowserRouter>
-=======
             // <BrowserRouter>
             //     <Routes>
             //         <Route path="/team" element={<TeamHome />}/>
@@ -82,7 +63,6 @@ function TeamPop(props) {
             <>
                 props.setTrigger(false)
             </>
->>>>>>> 0c89cd0e12e97e383a399fab5a6f1f9385aecd76
         )
     }
 
@@ -117,11 +97,7 @@ function TeamPop(props) {
                                 onChange={handleDescription}
                                 value={description}
                             />
-<<<<<<< HEAD
-                            <button onClick={openTeamPage}>Submit</button>
-=======
                             <button>Submit</button>
->>>>>>> 0c89cd0e12e97e383a399fab5a6f1f9385aecd76
                             </div>  
                         </form>
                         <button className="close" onClick={()=> props.setTrigger(false)}>
