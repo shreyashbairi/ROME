84b9012fe8eb679cd5b22b75724b69db90581484
noah1320 <54676274+noah1320@users.noreply.github.com>
2023-02-28 23:59:56 -0500

    Update AddEvent.jsx

--- client/src/components/jsx/AddEvent.jsx
+++ client/src/components/jsx/AddEvent.jsx
@@ -6,8 +6,18 @@ import Popup from "reactjs-popup";
 import 'reactjs-popup/dist/index.css';
 
 
+const AddEvent = (props) => {
+    const [popupOpened, setPopupOpened] = useState(false);
+    const [eventTitle, setEventTitle] = useState("");
+    const [eventDescription, setEventDescription] = useState("");
+    const [eventDate, setEventDate] = useState("");
+    const [eventStartTime, setEventStartTime] = useState("");
+    const [eventEndTime, setEventEndTime] = useState("");
 
-function AddEvent(props) {
+    function handleEventSubmit (e) {
+        e.preventDefault();
+        alert(eventTitle)
+    }
     async function handleSubmit(e) {
         e.preventDefault();
         try{
@@ -27,60 +37,89 @@ function AddEvent(props) {
     }
     return (props.trigger) ? (
 <>
-        <div class="loginpopup">
-            <div class="formPopup" id="popupForm">
-                
-            <h2>Add Team events</h2>
-
-            <div class="row mt-3">
-                <div class="col-sm-3">
-                <strong>Title</strong>
-                </div>
-                <div class="col-sm-9 text-secondary">
-                    <input type="event" class="form-control" id="eventtitle"  placeholder="Title" required />
-                </div>
+    <div class="loginpopup">
+        <div class="formPopup" id="popupForm">
+            
+        <h2>Add Team events</h2>
+        <form onSubmit={handleEventSubmit}>
+        <div class="row mt-3">
+            <div class="col-sm-3">
+               <strong>Title</strong>
             </div>
-            <div class="row mt-3">
-                <div class="col-sm-3">
-                <strong>Description</strong>
-                </div>
-                <div class="col-sm-9 text-secondary">
-                    <input type="description" class="form-control" id="eventdescription"  placeholder="Description" required />
-                </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-sm-3">
-                <strong>Date</strong>
-                </div>
-                <div class="col-sm-9 text-secondary">
-                    <input type="date" class="form-control" id="eventdate"  required />
-                </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col-sm-3">
-                <strong>Start time</strong>
-                </div>
-                <div class="col-sm-9 text-secondary">
-                    <input type="time" class="form-control" id="eventstarttime"  required />
-                </div>
-
-            </div>
-            <div class="row mt-3">
-
-                <div class="col-sm-3">
-                <strong>End time</strong>
-                </div>
-                <div class="col-sm-9 text-secondary">
-                    <input type="time" class="form-control" id="eventendtime"  required />
-                </div>
-                </div>
-
-                
-
-            <button type="submit" class="btn">Submit</button>
-            <button type="Cancel" class="btn cancel" onClick={()=> props.setTrigger()}>Cancel</button>
+            <div class="col-sm-9 text-secondary">
+                <input 
+                type="event" 
+                class="form-control" 
+                id="eventtitle"  
+                placeholder="Title"
+                value={eventTitle}
+                onChange={e => setEventTitle(e.target.value)} required />
             </div>
         </div>
+        <div class="row mt-3">
+            <div class="col-sm-3">
+               <strong>Description</strong>
+            </div>
+            <div class="col-sm-9 text-secondary">
+                <input 
+                type="description" 
+                class="form-control" 
+                id="eventdescription"  
+                placeholder="Description" 
+                value={eventDescription} 
+                onChange={e => setEventDescription(e.target.value)} required />
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-sm-3">
+               <strong>Date</strong>
+            </div>
+            <div class="col-sm-9 text-secondary">
+                <input 
+                type="date" 
+                class="form-control" 
+                id="eventdate"  
+                value={eventDate}
+                onChange={e => setEventDate(e.target.value)} required />
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-sm-3">
+               <strong>Start time</strong>
+            </div>
+            <div class="col-sm-9 text-secondary">
+                <input 
+                type="time" 
+                class="form-control" 
+                id="eventstarttime"  
+                value={eventStartTime}
+                onChange={e => setEventStartTime(e.target.value)} required />
+            </div>
+
+        </div>
+        <div class="row mt-3">
+
+            <div class="col-sm-3">
+               <strong>End time</strong>
+            </div>
+            <div class="col-sm-9 text-secondary">
+                <input 
+                type="time" 
+                class="form-control" 
+                id="eventendtime" 
+                value={eventEndTime}
+                onChange={e => setEventEndTime(e.target.value)} required />
+            </div>
+            </div>
+
+            
+
+        <button type="submit" class="btn">Submit</button>
+        </form>
+        <button type="Cancel" class="btn cancel" onClick={()=> props.setTrigger()}>Cancel</button>
+        </div>
+      </div>
+
       </>
     ) : "";
 }
