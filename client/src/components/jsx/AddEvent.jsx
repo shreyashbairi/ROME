b0ef68643f1575eed383f887a4468403d9f01e27
willcq12 <willcq12@gmail.com>
2023-02-28 22:19:00 -0500

    AddEvent grabs data

--- client/src/components/jsx/AddEvent.jsx
+++ client/src/components/jsx/AddEvent.jsx
@@ -5,6 +5,16 @@ import { useState } from "react";
 
 const AddEvent = () => {
     const [popupOpened, setPopupOpened] = useState(false);
+    const [eventTitle, setEventTitle] = useState("");
+    const [eventDescription, setEventDescription] = useState("");
+    const [eventDate, setEventDate] = useState("");
+    const [eventStartTime, setEventStartTime] = useState("");
+    const [eventEndTime, setEventEndTime] = useState("");
+
+    function handleEventSubmit (e) {
+        e.preventDefault();
+        alert(eventTitle)
+    }
 
     return (
 <>
@@ -12,13 +22,19 @@ const AddEvent = () => {
         <div class="formPopup" id="popupForm">
             
         <h2>Add Team events</h2>
-
+        <form onSubmit={handleEventSubmit}>
         <div class="row mt-3">
             <div class="col-sm-3">
                <strong>Title</strong>
             </div>
             <div class="col-sm-9 text-secondary">
-                <input type="event" class="form-control" id="eventtitle"  placeholder="Title" required />
+                <input 
+                type="event" 
+                class="form-control" 
+                id="eventtitle"  
+                placeholder="Title"
+                value={eventTitle}
+                onChange={e => setEventTitle(e.target.value)} required />
             </div>
         </div>
         <div class="row mt-3">
@@ -26,7 +42,13 @@ const AddEvent = () => {
                <strong>Description</strong>
             </div>
             <div class="col-sm-9 text-secondary">
-                <input type="description" class="form-control" id="eventdescription"  placeholder="Description" required />
+                <input 
+                type="description" 
+                class="form-control" 
+                id="eventdescription"  
+                placeholder="Description" 
+                value={eventDescription} 
+                onChange={e => setEventDescription(e.target.value)} required />
             </div>
         </div>
         <div class="row mt-3">
@@ -34,7 +56,12 @@ const AddEvent = () => {
                <strong>Date</strong>
             </div>
             <div class="col-sm-9 text-secondary">
-                <input type="date" class="form-control" id="eventdate"  required />
+                <input 
+                type="date" 
+                class="form-control" 
+                id="eventdate"  
+                value={eventDate}
+                onChange={e => setEventDate(e.target.value)} required />
             </div>
         </div>
         <div class="row mt-3">
@@ -42,7 +69,12 @@ const AddEvent = () => {
                <strong>Start time</strong>
             </div>
             <div class="col-sm-9 text-secondary">
-                <input type="time" class="form-control" id="eventstarttime"  required />
+                <input 
+                type="time" 
+                class="form-control" 
+                id="eventstarttime"  
+                value={eventStartTime}
+                onChange={e => setEventStartTime(e.target.value)} required />
             </div>
 
         </div>
@@ -52,13 +84,19 @@ const AddEvent = () => {
                <strong>End time</strong>
             </div>
             <div class="col-sm-9 text-secondary">
-                <input type="time" class="form-control" id="eventendtime"  required />
+                <input 
+                type="time" 
+                class="form-control" 
+                id="eventendtime" 
+                value={eventEndTime}
+                onChange={e => setEventEndTime(e.target.value)} required />
             </div>
             </div>
 
             
 
         <button type="submit" class="btn">Submit</button>
+        </form>
         <button type="button" class="btn cancel" >Cancel</button>
         </div>
       </div>
