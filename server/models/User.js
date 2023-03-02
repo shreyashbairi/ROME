14583e3efcb6e4a553e281167e51f22697ae112f
willcq12 <willcq12@gmail.com>
2023-03-01 20:44:52 -0500

    schema setup

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,32 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
+const TeamSchema = new Schema({
+    team: String,
+    description:String
+});
+
+const EventSchema = new Schema({
+
+});
+
+const PersonalTaskSchema = new Schema({
+
+});
+
+const ProfileSchema = new Schema({
+
+});
+
 const UserSchema = new Schema({
     userFullname: String,
     userEmail: {type:String, unique:true},
-    userUserName:String,
+    userUserName: String,
     userPassword: String,
+    userTeams: [TeamSchema],
+    userEvents: [EventSchema],
+    userPersonalTask: [PersonalTaskSchema],
+    userProfile: ProfileSchema,
 });
 
 const UserModel = mongoose.model('User', UserSchema);
