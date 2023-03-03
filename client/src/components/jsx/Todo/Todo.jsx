b22fcedc6093466e52e9d8135fdcde7ba1511fa2
noah1320 <54676274+noah1320@users.noreply.github.com>
2023-03-03 00:02:23 -0500

    Popup for details

--- client/src/components/jsx/Todo/Todo.jsx
+++ client/src/components/jsx/Todo/Todo.jsx
@@ -3,12 +3,33 @@ import  { AiFillCloseCircle } from 'react-icons/ai'
 import { TiEdit } from 'react-icons/ti'
 import TodoForm from './TodoForm1'
 import {BsThreeDots} from 'react-icons/bs'
+import Popup from 'reactjs-popup'
 
 function Todo({todos,completeTodo,removeTodo,editTask}) {
+    const [show, setShow] = useState(false);
+    const [edit, setEdit] = useState(false);
+
+
     const [change, setChange] = useState({
         id: null,
         value:''
     })
+
+    const openform = () => {
+        setShow(true);
+    };
+
+    const closeform = () => {
+        setShow(false);
+    }
+
+    const openedit = () => {
+        setEdit(true);
+    };
+
+    const closeedit = () => {
+        setEdit(false);
+    }
 
     const submitUpdate = value => {
         editTask(change.id, value)
@@ -81,13 +102,46 @@ function Todo({todos,completeTodo,removeTodo,editTask}) {
             {todos.map((todo,index)=>{
                 return(
                     <div key = {index} class="task">
-                        
+                        <Popup class="detailedTask" trigger={<button>
+                            {todo.title} {todo.date} </button>} open={show}
+                            onOpen={openform} position="right center" nested modal>
+                            <div class="card">
+                            <h2>Task details</h2>
+
+                            <div class="row mt-3">
+                                    <div class="col-sm-3">
+                                    <strong>Title</strong>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        {todo.title}
+                                    </div>
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col-sm-3">
+                                    <strong>Description</strong>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        {todo.description}
+                                    </div>
+                                </div>
+                                <div class="row mt-3">
+                                    <div class="col-sm-3">
+                                    <strong>Date</strong>
+                                    </div>
+                                    <div class="col-sm-9 text-secondary">
+                                        {todo.date}
+                                    </div>
+                                </div>
+                                <button type="Cancel" class="btn cancel" onClick={closeform}>Cancel</button>
+   
+                            </div>
+
+                        </ Popup>
                         <div key={todo.id} > 
                         {/* onClick={()=>completeTodo(todo.id)} was removed from above */}
-                        <button onClick={()=>displayDetails(todo.date, todo.description)}>
+                        {/* <button>
                             {todo.title} {todo.date}
-                            {/* {todo.date} */}
-                        </button>
+                        </button> */}
                             
                             
                         </div>
